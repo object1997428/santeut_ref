177b5ed0eac30845ff53bfb8183c70f54bdb38bc
object1997 <71408968+object1997428@users.noreply.github.com>
2025-08-21 11:56:13 +0900

    feat: Jwt 토큰 검증 기능 추가

--- hiking/src/main/java/com/santeut/hiking/controller/HikingController.java
+++ hiking/src/main/java/com/santeut/hiking/controller/HikingController.java
@@ -3,6 +3,7 @@ package com.santeut.hiking.controller;
 import com.santeut.hiking.service.HikingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
